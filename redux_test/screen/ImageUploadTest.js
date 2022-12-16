ed0efe8431b3c89cfa491d7b49f470a21ea7768c
a19950605 <a1995060569@gmail.com>
2022-12-16 19:10:42 +0800

    notes

--- redux_test/screen/ImageUploadTest.js
+++ redux_test/screen/ImageUploadTest.js
@@ -6,6 +6,15 @@ import {View, StyleSheet, Button} from 'react-native';
 import {TextInput} from 'react-native-paper';
 
 const ImageUploadTest = () => {
+  const [test, setText] = usseState();
+
+  /**
+   * image upload
+   * preview in a container
+   * zoom the image
+   * click on the image and show the position
+   * add the pin on image
+   */
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>image select test</Text>
