30013b9fe938df99585c496e539c87fdf0845363
a19950605 <a1995060569@gmail.com>
2022-12-23 17:09:36 +0800

    MENU

--- redux_test/screen/components/UserAccountCreate.js
+++ redux_test/screen/components/UserAccountCreate.js
@@ -3,141 +3,164 @@ import {Input, Icon} from '@rneui/themed';
 
 import {View, StyleSheet, TouchableOpacity, Text} from 'react-native';
 import {TextInput, Button, Menu, Divider, Provider} from 'react-native-paper';
+
 const UserAccountCreate = () => {
-  const [visible, setVisible] = React.useState(false);
-
-  const openMenu = () => setVisible(true);
-
-  const closeMenu = () => setVisible(false);
-
   //user name
   //displayname
   //staffid
   //role (dropdown) default text first password, password confirmation remarks
   //status  active disabled
   return (
-    <View style={{backgroundColor: 'white', flex: 1}}>
-      <View style={{padding: 10}}>
-        <View
-          style={{
-            flexDirection: 'row',
-            width: '100%',
-            alignItems: 'center',
-            marginBottom: 15,
-          }}>
-          <Icon
-            name="monitor"
-            size={24}
-            color="black"
-            style={{padding: 10, justifyContent: 'center'}}
-          />
-          <TextInput
-            selectTextOnFocus={false}
-            style={{width: '85%', backgroundColor: 'transparent'}}
-            label="Controller ID"
-            value={''}
-            onChangeText={''}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            width: '100%',
-            alignItems: 'center',
-            marginBottom: 15,
-          }}>
-          <Icon
-            name="hash"
-            size={24}
-            color="black"
-            type="feather"
-            style={{padding: 10}}
-          />
-          <TextInput
-            selectTextOnFocus={false}
-            style={{width: '85%', backgroundColor: 'transparent'}}
-            label="Device ID"
-            value={''}
-            onChangeText={''}
-          />
-        </View>
-
-        <View
-          style={{
-            flexDirection: 'row',
-            width: '100%',
-            alignItems: 'center',
-            marginBottom: 15,
-          }}>
-          <Icon
-            name="call-split"
-            size={24}
-            color="black"
-            type="material"
-            style={{padding: 10}}
-          />
-          <TextInput
-            selectTextOnFocus={false}
-            style={{width: '85%', backgroundColor: 'transparent'}}
-            label="Relay Channel Index"
-            value={''}
-            onChangeText={''}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            width: '100%',
-            alignItems: 'center',
-            marginBottom: 15,
-          }}>
-          <Icon
-            name="play"
-            size={24}
-            color="black"
-            type="fontisto"
-            style={{padding: 10}}
-          />
-          <TextInput
-            selectTextOnFocus={false}
-            style={{width: '85%', backgroundColor: 'transparent'}}
-            label="Status"
-            value={''}
-            onChangeText={''}
-          />
-        </View>
-        <View
-          style={{
-            backgroundColor: 'white',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            padding: 20,
-          }}>
-          <TouchableOpacity
+    <Provider>
+      <View style={{backgroundColor: 'white', flex: 1}}>
+        <View style={{padding: 10}}>
+          <View
             style={{
               flexDirection: 'row',
+              width: '100%',
               alignItems: 'center',
-              borderColor: 'green',
-              borderWidth: 1,
-              borderRadius: 5,
-              padding: 10,
-            }}
-            onPress={() => {
-              alert('hello');
+              marginBottom: 15,
             }}>
             <Icon
-              name="md-save-sharp"
-              type="ionicon"
+              name="monitor"
               size={24}
-              color="green"
-              style={{justifyContent: 'center', paddingRight: 5}}
+              color="black"
+              style={{padding: 10, justifyContent: 'center'}}
             />
-            <Text style={{color: 'green'}}> Save</Text>
-          </TouchableOpacity>
+            <TextInput
+              selectTextOnFocus={false}
+              style={{width: '85%', backgroundColor: 'transparent'}}
+              label="Controller ID"
+              value={''}
+              onChangeText={''}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              width: '100%',
+              alignItems: 'center',
+              marginBottom: 15,
+            }}>
+            <Icon
+              name="hash"
+              size={24}
+              color="black"
+              type="feather"
+              style={{padding: 10}}
+            />
+            <TextInput
+              selectTextOnFocus={false}
+              style={{width: '85%', backgroundColor: 'transparent'}}
+              label="Device ID"
+              value={''}
+              onChangeText={''}
+            />
+          </View>
+
+          <View
+            style={{
+              flexDirection: 'row',
+              width: '100%',
+              alignItems: 'center',
+              marginBottom: 15,
+            }}>
+            <Icon
+              name="call-split"
+              size={24}
+              color="black"
+              type="material"
+              style={{padding: 10}}
+            />
+            <TextInput
+              selectTextOnFocus={false}
+              style={{width: '85%', backgroundColor: 'transparent'}}
+              label="Relay Channel Index"
+              value={''}
+              onChangeText={''}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              width: '100%',
+              alignItems: 'center',
+              marginBottom: 15,
+            }}>
+            <Icon
+              name="play"
+              size={24}
+              color="black"
+              type="fontisto"
+              style={{padding: 10}}
+            />
+            <TextInput
+              selectTextOnFocus={false}
+              style={{width: '85%', backgroundColor: 'transparent'}}
+              label="Status"
+              value={''}
+              onChangeText={''}
+            />
+            <MyComponent />
+          </View>
+          <View
+            style={{
+              backgroundColor: 'white',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+              padding: 20,
+            }}>
+            <TouchableOpacity
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                borderColor: 'green',
+                borderWidth: 1,
+                borderRadius: 5,
+                padding: 10,
+              }}
+              onPress={() => {
+                alert('hello');
+              }}>
+              <Icon
+                name="md-save-sharp"
+                type="ionicon"
+                size={24}
+                color="green"
+                style={{justifyContent: 'center', paddingRight: 5}}
+              />
+              <Text style={{color: 'green'}}> Save</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
+    </Provider>
+  );
+};
+
+const MyComponent = () => {
+  const [visible, setVisible] = React.useState(false);
+
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
+
+  return (
+    <Menu
+      style={{backgroundColor: 'white'}}
+      visible={visible}
+      onDismiss={closeMenu}
+      anchor={
+        <Button onPress={openMenu}>
+          <Text>Show menu</Text>
+        </Button>
+      }>
+      <Menu.Item onPress={() => {}} title="Item 1" />
+      <Menu.Item onPress={() => {}} title="Item 2" />
+      <Divider />
+      <Menu.Item onPress={() => {}} title="Item 3" />
+    </Menu>
   );
 };
 
