e82da74a75eedb06ed24d650664d80b9dad2ae10
a19950605 <a1995060569@gmail.com>
2023-02-06 19:54:37 +0800

    open status modal

--- redux_test/src/screen/Monitoring/components/MonitoringFilterModal.js
+++ redux_test/src/screen/Monitoring/components/MonitoringFilterModal.js
@@ -79,10 +79,78 @@ export const MonitoringFilterModal = ({
     </View>
   );
 };
-
+//拆出黎
 const StatusModal = ({
   setOpenStatusModal,
   openStatusModal,
+  filterStatus,
+  setFilterStatus,
+}) => {
+  const options = [
+    {displayValue: 'All', stateVal: ''},
+    {displayValue: 'ACTIVE', stateVal: 'ACTIVE'},
+
+    {displayValue: 'Isolated', stateVal: 'SPECIAL'},
+    {displayValue: 'Maintenance', stateVal: 'DISABLED'},
+  ];
+  return (
+    <View>
+      <Modal
+        isVisible={openStatusModal}
+        customBackdrop={
+          <TouchableWithoutFeedback
+            onPress={() => {
+              setOpenStatusModal(false);
+            }}>
+            <View style={{flex: 1, backgroundColor: 'black'}} />
+          </TouchableWithoutFeedback>
+        }>
+        <View style={{flex: 1, backgroundColor: 'white', padding: 20}}>
+          <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity
+              onPress={() => {
+                setOpenStatusModal(false);
+              }}
+              style={{
+                marginRight: 10,
+                borderWidth: 1,
+                borderColor: 'blue',
+                borderRadius: 5,
+                padding: 10,
+              }}>
+              <Text style={{fontSize: 18, color: 'blue'}}>Done</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View>
+            {options.map(o => {
+              return (
+                <TouchableOpacity
+                  onPress={() => {
+                    setFilterStatus(o.displayValue);
+                  }}
+                  key={o.displayValue}
+                  style={{
+                    marginRight: 10,
+
+                    padding: 10,
+                  }}>
+                  <Text style={{fontSize: 18, color: 'blue'}}>
+                    {o.displayValue}
+                  </Text>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const ConnStatusModal = ({
+  setOpenConnModal,
+  openConnModal,
   filterStatus,
   setFilterStatus,
 }) => {
